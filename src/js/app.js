94d8126e941a93367ef60b312db72fe3c3a13989
Cgeorge1807 <christian03george@gmail.com>
2025-01-15 16:52:12 +0000

    Ternarios

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,27 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1> ${variables.name} ${variables.lastName} </h1>
-          <h2> ${variables.role}</h2>
-          <h3> ${variables.city}, ${variables.country}</h3>
+          <h1> ${variables.name == null ? "Nombre" : variables.name} 
+          ${variables.lastName == null ? "Apellido" : variables.lastName} </h1>
+          <h2> ${
+            variables.role == null ? "Role" : variables.role
+          }</h2>          
+            <h3> ${variables.city == null ? "City" : variables.city}, ${
+    variables.country == null ? "Country" : variables.country
+  }</h3>
           <ul class=" ${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "4Geeks" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "4Geeks" : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin == null ? "4Geeks" : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null ? "4Geeks" : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -64,11 +77,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: "Nombre",
-    lastName: "Apellido",
-    role: "Rol",
-    country: "Pais",
-    city: "Ciudad"
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
   };
   render(window.variables); // render the card for the first time
 
